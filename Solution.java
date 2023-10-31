3cabb34d76a859b52a69ec09d4e22a6c134085ac
Lam <sarah.lam@fiserv.com>
2023-10-31 14:23:33 -0400

    AH5 Completed

--- Solution.java
+++ Solution.java
@@ -1,12 +1,30 @@
+import java.util.*;
 class Solution {
     public static char mostFrequentChar(String s) {
-      
-      return;
+      HashMap<Character, Integer> mp = new HashMap<>();
+      int len = s.length();
+      char ans = 0;
+      int count = 0;
+
+      for(int i = 0; i < len; i++){
+        char c = s.charAt(i);
+        mp.put(c, mp.getOrDefault(c, 0) + 1);
+        if(count < mp.get(c)){
+          ans = c;
+          count = mp.get(c);
+        }
+      }
+      // Solution word = new Solution();
+      // char c;
+      // for(int i = 0; i < s.length(); i++){
+      //   if()
+      // }
+      return ans;
     }
   
     public static void main(String[] args) {
       // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("mississippi"));
     }
 
   }
